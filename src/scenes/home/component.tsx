decc870462e7a03a24a2f0fd91e4bb70afd08ded
Thomas Monfre <tmonfre1@gmail.com>
2021-10-09 11:38:40 -0500

    feat: add docker-compose.yml + connect to server

--- src/scenes/home/component.tsx
+++ src/scenes/home/component.tsx
@@ -1,7 +1,19 @@
-const Home = () => (
-  <div id="home-container">
-    <h1>Home</h1>
-  </div>
-);
+import { useState, useEffect } from 'react';
+import { getText } from '../../utils';
+
+const Home = () => {
+  const [text, setText] = useState<string>('');
+
+  useEffect(() => {
+    getText().then(setText);
+  }, []);
+
+  return (
+    <div id="home-container">
+      <h1>Home</h1>
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default Home;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,11 @@
-export {};
+import axios from 'axios';
+
+/* eslint-disable import/prefer-default-export */
+async function getText(): Promise<string> {
+  const { data } = await axios.get('http://localhost:9090');
+  return data;
+}
+
+export {
+  getText,
+};
